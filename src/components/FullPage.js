b1d53ca7d6c3800e74de8a866aa1c9d7f8109a5c
Marius Hendriks <mark.hendriks9999@gmail.com>
2020-03-10 20:02:18 +0100

    :package: added fontawesome

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -1,32 +1,34 @@
 import React from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const Fullpage = () => (
-    <ReactFullpage
-        //fullpage options
-        licenseKey={"YOUR_KEY_HERE"}
-        scrollingSpeed={1000} /* Options here */
-        render={({ state, fullpageApi }) => {
-            return (
-                <ReactFullpage.Wrapper>
-                    <div className="section">
-                        <p>Section 1 (welcome to fullpage.js)</p>
-                        <button onClick={() => fullpageApi.moveSectionDown()}>
-                            Click me to move down
-                        </button>
-                    </div>
-                    <div className="section">
-                        <div class="slide">Slide 2.1</div>
-                        <div class="slide">Slide 2.2</div>
-                        <div class="slide">Slide 2.3</div>
-                    </div>
-                    <div className="section">
-                        <p>Section 3</p>
-                    </div>
-                </ReactFullpage.Wrapper>
-            );
-        }}
-    />
+  <ReactFullpage
+    //fullpage options
+    licenseKey={"YOUR_KEY_HERE"}
+    scrollingSpeed={1000} /* Options here */
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <h1>Intake design acedamy</h1>
+            <div onClick={() => fullpageApi.moveSectionDown()}>
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="section">
+            <div class="slide">Slide 2.1</div>
+            <div class="slide">Slide 2.2</div>
+            <div class="slide">Slide 2.3</div>
+          </div>
+          <div className="section">
+            <p>Section 3</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
 );
 
 export default Fullpage;
